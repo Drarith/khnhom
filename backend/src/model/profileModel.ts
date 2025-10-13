b111daaf4afabd97d7a4b27b8368fae056a81348
Dararith <markwilhelm5540@gmail.com>
2025-10-13 21:34:55 +0700

    added profile and user model

---
+++ backend/src/model/profileModel.ts
@@ -0,0 +1,55 @@
+import mongoose from "mongoose";
+
+const { Schema } = mongoose;
+
+const profileSchema = new Schema({
+  user: {
+    type: Schema.Types.ObjectId,
+    ref: 'User',
+    required: true
+  },
+  username: {
+    type: String,
+    required: true,
+    unique: true,
+    trim: true
+  },
+  displayName: {
+    type: String,
+    required: true
+  },
+  bio: {
+    type: String,
+    default: ''
+  },
+  profilePictureUrl: {
+    type: String,
+    default: ''
+  },
+  paymentQrCodeUrl: {
+    type: String,
+    default: ''
+  },
+
+  links: [{
+    title: { type: String, required: true },
+    url: { type: String, required: true }
+  }],
+  socials: {
+    facebook: { type: String, default: '' },
+    instagram: { type: String, default: '' },
+    telegram: { type: String, default: '' },
+    youtube: { type: String, default: '' },
+    linkedIn: { type: String, default: '' },
+    x: { type: String, default: '' },
+    tiktok: { type: String, default: '' },
+    github: { type: String, default: '' },
+    
+  },
+  theme: {
+    type: String,
+    default: 'default'
+  }
+}, { timestamps: true });
+
+export default mongoose.model("Profile", profileSchema);
---
+++ backend/src/model/userModel.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose";
+import bcrypt from "bcrypt";
+
+const { Schema } = mongoose;
+
+const userSchema = new Schema({
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    lowercase: true,
+    trim: true
+  },
+  password: {
+    type: String,
+    required: true
+  },
+  isSupporter: {
+    type: Boolean,
+    default: false
+  }
+}, { timestamps: true });
+
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  const salt = await bcrypt.genSalt(10);
+  this.password = await bcrypt.hash(this.password, salt);
+  next();
+});
+
+userSchema.methods.comparePassword = async function (candidatePassword: string) {
+  return bcrypt.compare(candidatePassword, this.password);
+};
+
+export default mongoose.model("User", userSchema);
