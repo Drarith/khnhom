0a7f3e7f9f35949ea8bf9cda3115848ff419dbb3
Dararith <markwilhelm5540@gmail.com>
2025-10-14 14:58:38 +0700

    nodemon now working in docker as expected

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -1,10 +1,10 @@
-import express from 'express';
-import mongoose from 'mongoose';
+import express from "express";
+import mongoose from "mongoose";
 
-import dotenv from 'dotenv';
+import dotenv from "dotenv";
 
-import type { Env } from './types/myENV.js';
-import User from './model/userModel.js';
+import type { Env } from "./types/myENV.js";
+import User from "./model/userModel.js";
 
 // Initialize the Express application
 const app = express();
@@ -13,26 +13,26 @@ const port = process.env.PORT || 3000;
 dotenv.config();
 
 const env: Env = {
-  MONGO_URI: process.env.MONGO_URI || '',
+  MONGO_URI: process.env.MONGO_URI || "",
   PORT: Number(process.env.PORT) || 3000,
 };
 
 // Connect to MongoDB
-mongoose.connect(env.MONGO_URI)
-.then(() => {
-  console.log('[database]: Connected to MongoDB');
-})
-.catch((error) => {
-  console.error('[database]: Error connecting to MongoDB', error);
-});
+mongoose
+  .connect(env.MONGO_URI)
+  .then(() => {
+    console.log("[database]: Connected to MongoDB");
+  })
+  .catch((error) => {
+    console.error("[database]: Error connecting to MongoDB", error);
+  });
 
 app.use(express.urlencoded({ extended: true }));
 
 // Define a simple route
-app.get('/', async (req, res) => {
-  res.send('Hello from Express + TypeScript!');
+app.get("/", async (req, res) => {
+  res.send("Hello from Express + TypeScript ");
 });
-
 
 app.listen(port, () => {
   console.log(`[server]: Server is running at http://localhost:${port}`);
