9c3ec889f85b70d0615cc04b741a3ed6d5cb9d42
Dararith <markwilhelm5540@gmail.com>
2025-12-08 19:19:50 +0700

    added socials

--- frontend/domnor/components/userProfile/UserProfile.tsx
+++ frontend/domnor/components/userProfile/UserProfile.tsx
@@ -1,33 +1,64 @@
 import { ProfileData } from "@/types/profileData";
 import Image from "next/image";
+import {
+  SiTiktok,
+  SiX,
+  SiTelegram,
+  SiInstagram,
+  SiYoutube,
+  SiGithub,
+  SiFacebook,
+} from "@icons-pack/react-simple-icons";
+
+type SocialPlatform =
+  | "facebook"
+  | "x"
+  | "instagram"
+  | "tiktok"
+  | "telegram"
+  | "youtube"
+  | "github";
+
+const icons: Record<SocialPlatform, React.ReactElement> = {
+  facebook: <SiFacebook color="black" />,
+  x: <SiX color="black" />,
+  instagram: <SiInstagram color="black" />,
+  tiktok: <SiTiktok color="black" />,
+  telegram: <SiTelegram color="black" />,
+  youtube: <SiYoutube color="black" />,
+  github: <SiGithub color="black" />,
+};
 
 export default function UserProfile({ data }: { data: ProfileData }) {
-  const backgorund = "primary"
   return (
-    <div className="min-h-screen bg-accent">
+    <div className="min-h-screen bg-primary">
       <div className="relative w-full">
         <div className="relative w-full h-96 md:h-[500px]">
-          <Image 
-            src={data.profilePictureUrl} 
-            alt="profile picture" 
+          <Image
+            src={data.profilePictureUrl}
+            alt="profile picture"
             fill
             className="object-cover"
             priority
           />
         </div>
-        
-        {/* Gradient Overlay - Fades to background color */}
-        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-accent" />
-        
-        {/* Optional: Stronger fade at bottom */}
-        <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-accent to-transparent" />
+
+        <div className="absolute inset-0 bg-gradient-to-b from-transparent via-transparent to-primary" />
+        <div className="absolute bottom-0 left-0 right-0 h-32 bg-gradient-to-t from-primary to-transparent" />
       </div>
 
-      
       <div className="relative -mt-20 px-4">
-        <h1 className="text-2xl font-bold text-center">
-          {data.displayName}
-        </h1>
+        <h1 className="text-2xl font-bold text-center">{data.displayName}</h1>
+        <div className="flex flex-row gap-2 justify-center">
+          {/* filter and map */}
+          {Object.entries(data.socials)
+            .filter(([_k, v]) => v !== "")
+            .map(([key, url]) => (
+              <a key={key} href={url} target="_blank" rel="noopener noreferrer">
+                {icons[key as SocialPlatform]}
+              </a>
+            ))}
+        </div>
       </div>
     </div>
   );
