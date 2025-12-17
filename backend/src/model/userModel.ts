e52fe023626c422fa70f61e12afa3a3bfff24809
Dararith <markwilhelm5540@gmail.com>
2025-12-17 14:53:18 +0700

    added gold supporter in user

--- backend/src/model/userModel.ts
+++ backend/src/model/userModel.ts
@@ -30,6 +30,10 @@ const userSchema = new Schema(
       type: Boolean,
       default: false,
     },
+    isGoldSupporter: {
+      type: Boolean,
+      default: false,
+    },
     isActive: {
       type: Boolean,
       default: true,
