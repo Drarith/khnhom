74221f7cbbf558de3dd1927ac01e8aa6aa0de7b9
Dararith <markwilhelm5540@gmail.com>
2025-12-19 00:36:53 +0700

    added more bg image

--- frontend/domnor/config/background.ts
+++ frontend/domnor/config/background.ts
@@ -14,5 +14,14 @@ export const backgroundImages = [
     {
         name: "concrete",
         url: "https://res.cloudinary.com/dosj9q3zb/image/upload/v1766044204/concrete_qot5vy.png"
+    },
+    {
+        name: "Deep Space Nebula",
+        url: "https://res.cloudinary.com/dosj9q3zb/image/upload/v1766079064/nebula_s0jlhh.png"
+
+    },
+    {
+        name: "Cyberpunk",
+        url: "https://res.cloudinary.com/dosj9q3zb/image/upload/v1766079063/cyberpunk_gw2vwn.png"
     }
 ]
